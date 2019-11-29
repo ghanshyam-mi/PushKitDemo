46eaa5c8485399b7fbf9da17aee20163c5b5cdd8
mac-00020 <ghanshyam.bhesaniya@mindinventory.com>
2019-11-29 18:02:30 +0530

    initial comment

--- PuskKitDemo/AppDelegate.swift
+++ PuskKitDemo/AppDelegate.swift
@@ -7,31 +7,117 @@
 //
 
 import UIKit
+import PushKit
+import CallKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    let window = UIWindow(frame: UIScreen.main.bounds)
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.registerForPushNotifications()
+        voipRegistration()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    // Register for VoIP notifications
+    func voipRegistration() {
+        
+        // Create a push registry object
+        let mainQueue = DispatchQueue.main
+        let voipRegistry: PKPushRegistry = PKPushRegistry(queue: mainQueue)
+        voipRegistry.delegate = self
+        voipRegistry.desiredPushTypes = [PKPushType.voIP]
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    // Push notification setting
+    func getNotificationSettings() {
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                UNUserNotificationCenter.current().delegate = self
+                guard settings.authorizationStatus == .authorized else { return }
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        } else {
+            let settings = UIUserNotificationSettings(types: [.sound, .alert, .badge], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+            UIApplication.shared.registerForRemoteNotifications()
+        }
     }
-
-
+    
+    // Register push notification
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) {
+                [weak self] granted, error in
+                guard let _ = self else {return}
+                guard granted else { return }
+                self?.getNotificationSettings()
+        }
+    }
 }
+
+// MARK:- UNUserNotificationCenterDelegate
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        let userInfo = response.notification.request.content.userInfo
+        print("didReceive ======", userInfo)
+        completionHandler()
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        let userInfo = notification.request.content.userInfo
+        print("willPresent ======", userInfo)
+        completionHandler([.alert, .sound, .badge])
+    }
+}
+
+//MARK: - PKPushRegistryDelegate
+extension AppDelegate : PKPushRegistryDelegate {
+    
+    // Handle updated push credentials
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate credentials: PKPushCredentials, for type: PKPushType) {
+        print(credentials.token)
+        let deviceToken = credentials.token.map { String(format: "%02x", $0) }.joined()
+        print("pushRegistry -> deviceToken :\(deviceToken)")
+    }
+        
+    func pushRegistry(_ registry: PKPushRegistry, didInvalidatePushTokenFor type: PKPushType) {
+        print("pushRegistry:didInvalidatePushTokenForType:")
+    }
+    
+    // Handle incoming pushes
+    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType) {
+         print(payload.dictionaryPayload)
+        
+        let payloadDict = payload.dictionaryPayload["aps"] as? [String:Any] ?? [:]
+        let message = payloadDict["alert"] as! String
+        //present a local notifcation to visually see when we are recieving a VoIP Notification
+        if UIApplication.shared.applicationState == UIApplication.State.active {
+            DispatchQueue.main.async() {
+               let alert = UIAlertController(title: "VoIP Demo", message: message, preferredStyle: .alert)
+               alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+               self.window.rootViewController?.present(alert, animated: true, completion: nil)
+            }
+         } else {
+            let content = UNMutableNotificationContent()
+            content.title = "VoIPDemo"
+            content.body = message
+            content.badge = 0
+            content.sound = UNNotificationSound.default
+                
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+            let request = UNNotificationRequest(identifier: "VoIPDemoIdentifier", content: content, trigger: trigger)
+            UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+         }
+    }
+}
+
 
